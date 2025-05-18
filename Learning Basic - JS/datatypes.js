aadc843057dfdf8384bb07526c57fb74cf092a7f
peth16 <petharavind16@gmail.com>
2025-05-18 13:32:12 +0530

    Add the data types learning html and JS folders

---
+++ Learning Basic - JS/datatypes.js
@@ -0,0 +1,11 @@
+// Primitive Data Types - Number, String, Boolean, Undefined, Null
+
+// Non-Primitive Data Types - Object, Array, Function, Date, RegExp
+
+// Primitive Data Types
+
+// Number
+
+var num = 120;
+
+console.log( num ); 
