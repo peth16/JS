d54b903a69c4fab147562df0898b4e9a553d3230
peth16 <petharavind16@gmail.com>
2025-05-18 13:21:42 +0530

    Added the Identifier and the variables declaration and printing statements files

---
+++ Learning Basic - JS/identifier.js
@@ -0,0 +1,72 @@
+let uname = "First Script";
+let cname = "Second Script";
+
+console.log(uname, cname);
+
+let _name = "Single Page Application";
+let $name = "SPA";
+let name0 = "Name"; // Numeric value is not allowed at the start of the variable name
+
+console.log(_name, $name);
+
+// Camel Case
+// Pascal Case
+// Underscore Case
+
+let newEmployeeId = 10; // Camel Case
+let NewEmployeeId = 20; // Pascal Case
+let new_employee_id = 30; // Underscore Case
+
+var age = 30;  //Declaration with initialization
+
+var age ; //Declaration without initialization
+
+age = 30; //Initialization
+
+var age = 70; //Re-declaration
+
+age = "Peth Aravind"; //Re-initialization
+
+console.log( age ); 
+
+// let variable
+
+let newAge; //Declaration without initialization
+
+newAge = 30; //Initialization
+
+//let newAge = 70; //Re-declaration is not allowed
+
+newAge = "Peth Aravind"; //Re-declaration is allowed
+
+console.log( age);
+
+// const variable
+
+//const employeeName; //Declaration without initialization is not allowed
+
+const employeeName = "Peth Aravind"; //Declaration with initialization
+
+//  employeeName = 10; //Constant variable re-initialization is not allowed
+
+console.log( employeeName );
+
+// Printing Statements
+
+//alert( employeeName ); //Alert box
+
+//document.write( employeeName ); //Write to HTML document
+
+//confirm( employeeName ); //Confirmation box
+
+//let userName = prompt( "Enter your name" ); //Prompt box
+
+//console.log( userName ); //Print to console
+
+let userID;
+
+console.error( userID ); //Print to console error
+
+console.warn( userID ); //Print to console warning
+
+console.clear(); //Clear console
